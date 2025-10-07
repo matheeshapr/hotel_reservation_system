742b5de903a165840cd0e452f9557853460a6e2d
matheeshapr <matheeshap62@gmail.com>
2025-10-07 09:42:43 +0530

    Add room and customer action methods; update UI styles and structure

--- src/main/java/Controller/CustomerInfomation.java
+++ src/main/java/Controller/CustomerInfomation.java
@@ -1,4 +1,11 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+
 public class CustomerInfomation {
+    public void roomaction(ActionEvent actionEvent) {
+    }
+
+    public void custaction(ActionEvent actionEvent) {
+    }
 }
